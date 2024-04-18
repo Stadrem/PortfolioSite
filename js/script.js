978a8bcc580d3ad5cb66257cba6ed8a5142780a3
Stadrem <106669174+Stadrem@users.noreply.github.com>
2024-04-18 20:11:23 +0900

    Design update
    1. Tilt effect when hovering over the card
    2. 3D Canvas Improvements
    3. Add Text content

--- js/script.js
+++ js/script.js
@@ -19,4 +19,24 @@ $("#babylonjs-close-btn").click(async function () {
     $('#babylon-viewer').toggle();
 })
 
+const tiltableDivs = document.querySelectorAll('.col');
+
+tiltableDivs.forEach(tiltableDiv => {
+    tiltableDiv.addEventListener('mousemove', (e) => {
+        const rect = tiltableDiv.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+        const maxTilt = 10;
+
+        const tiltX = (mouseX - rect.width / 2) / (rect.width / 2) * maxTilt;
+        const tiltY = (mouseY - rect.height / 2) / (rect.height / 2) * -maxTilt;
+
+        tiltableDiv.style.transform = `perspective(1000px) rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
+    });
+
+    tiltableDiv.addEventListener('mouseleave', () => {
+        tiltableDiv.style.transform = 'none';
+    });
+});
+
 AOS.init();
