8524ce38e83d6187d9f54ab159316fdab6d2882b
Stadrem <106669174+Stadrem@users.noreply.github.com>
2024-04-28 15:01:06 +0900

    Add game portfolio
    008.jpg

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-$("#btnAbout").click(async function () {
-    $('#aboutTextToggle').toggle();
-})
-
-$("#btnCM").click(async function () {
-    $('#cmToggle').toggle();
-})
-
-$("#btnGM").click(async function () {
-    $('#gmToggle').toggle();
-    $('#sectionCap').toggle();
-})
-
 $("#babylonjs-run-btn").click(async function () {
     $('#babylon-viewer').toggle();
 })
